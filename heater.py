c6c598f1bc90ed41d7146eab01d0666b9879e73f
Bjorn Hansen <holomorph@gmail.com>
2018-03-16 22:43:15 -0700

    impliment heater methods

--- heater.py
+++ heater.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+from __future__ import print_function
+import RPi.GPIO as GPIO
 
 class HeaterRelay(object):
-    def __init__(self, relay_pin):
-        pass
+    def __init__(self, relay_pin, board = GPIO.BCM):
+        self.relay_pin = relay_pin
+        GPIO.setmode(board)
+        GPIO.setup(self.relay_pin, GPIO.OUT)
     def on(self):
-        pass
+        GPIO.output(self.relay_pin, GPIO.HIGH)
     def off(self):
-        pass
+        GPIO.output(self.relay_pin, GPIO.LOW)

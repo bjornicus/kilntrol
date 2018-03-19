2454985eec63729ccde35c961123c9ac32ec2a8e
Bjorn Hansen <holomorph@gmail.com>
2018-03-19 07:46:18 -0700

    add glaze_profile

--- profiles.py
+++ profiles.py
@@ -1,7 +1,21 @@
 MINUTES = 60
+HOURS = 60 * MINUTES
+
 sample_profile = [
     [0, 70],
     [10 * MINUTES, 90],
     [20 * MINUTES, 130],
     [30 * MINUTES, 120]
+]
+
+
+glaze_profile = [
+    [0, 65],
+    [2 * HOURS , 550],
+    [4 * HOURS , 1000],
+    [6 * HOURS , 1900],
+    [7 * HOURS , 2010],
+    [7 * HOURS + 40 * MINUTES , 2010],
+    [8 * HOURS , 2120],
+    [10 * HOURS , 1200]
 ]

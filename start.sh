6f012f6446a88953566909f8be961cb23f86d12b
Bjorn Hansen <holomorph@gmail.com>
2018-03-19 07:47:47 -0700

    tee logs from start script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo starting kilntrol
-python3 kilntrol.py &
+python3 kilntrol.py | tee logs/kilntrol.log &
 
 sleep 1
 echo starting log uploader
-python upload_logs.py &
+python upload_logs.py | tee logs/upload.log &
 
 echo started processes: 
 jobs -p

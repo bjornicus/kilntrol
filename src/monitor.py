d0914d1d3c6be87cebf289d5bda1de4902241dc9
Bjorn Hansen <bh@hoylu.com>
2020-12-12 10:43:00 -0800

    monitor.py works as intended and even cleans up GPIO on exit

--- src/monitor.py
+++ src/monitor.py
@@ -1,15 +1,30 @@
+import signal
+import sys
+import time
+
 
 def main():
     """ Run KilnTrol """
-    import sys
     from clocks import BasicClock as Clock
     from max31855 import MAX31855
     from target_profile import TargetProfile
     from loggers import FileLogger as Logger
+
+    temperature = MAX31855(cs_pin=27, clock_pin=22, data_pin=17, units="f")
+
+    def signal_handler(sig, frame):
+        print('cleaning up GPIO...')
+        temperature.cleanup()
+        exit()
+    signal.signal(signal.SIGINT, signal_handler)
+
     tick_interval = 5
     clock = Clock()
-    temperature = MAX31855(cs_pin=27, clock_pin=22, data_pin=17, units="f")
-    while clock.now() < t_stop:
+    logger = Logger('logs/temperature')
+
+    while True:
+        t = temperature.get()
+        print('logging {0}'.format(t))
         logger.log(clock.now(), temperature.get(), 0)
         time.sleep(tick_interval)
 

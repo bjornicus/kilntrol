215cf4cb7d695564baf5ad530beb61074278a011
Bjorn Hansen <bh@hoylu.com>
2020-12-12 09:10:17 -0800

    add monitor only script (untested)

---
+++ src/monitor.py
@@ -0,0 +1,14 @@
+
+def main():
+    """ Run KilnTrol """
+    import sys
+    from clocks import BasicClock as Clock
+    from max31855 import MAX31855
+    from target_profile import TargetProfile
+    from loggers import FileLogger as Logger
+    tick_interval = 5
+    clock = Clock()
+    temperature = MAX31855(cs_pin=27, clock_pin=22, data_pin=17, units="f")
+    while clock.now() < t_stop:
+        logger.log(clock.now(), temperature.get(), 0)
+        time.sleep(tick_interval)

f7e784caf2a8f67ccbd38474f59c627b0c575ce3
Bjorn Hansen <holomorph@gmail.com>
2021-11-27 11:21:46 -0800

    rm uneeded clock

--- src/clocks.py
+++ src/clocks.py
@@ -1,34 +1,16 @@
 import time
 import os.path
-from heater_sim import TICKS_PER_SECOND
-
-
 class BasicClock(object):
-    def __init__(self, start_time=time.time()):
-        self.start = start_time
+    def __init__(self, start_offset_seconds=0):
+        self.start = time.time() + start_offset_seconds
 
     def now(self):
         return time.time() - self.start
 
-
-class PersistantClock(BasicClock):
-    def __init__(self, start_time_file_name='start_time.txt'):
-        if os.path.isfile(start_time_file_name):
-            print('loading start time from file ' + start_time_file_name)
-            with open(start_time_file_name) as f:
-                start_time = float(f.readline())
-                super().__init__(start_time)
-        else:
-            print('no persisted start time found, starting from now.')
-            start_time = time.time()
-            with open(start_time_file_name, 'w') as f:
-                f.write(str(start_time) + '\n')
-            super().__init__(start_time)
-
-
-class SpeedySimClock(object):
-    def __init__(self, start_time=time.time()):
-        self.start = start_time
+class SimClock(object):
+    def __init__(self, start_offset_seconds=0, simulation_speed=1):
+        self.start = time.time() + start_offset_seconds
+        self.simulation_speed = simulation_speed
 
     def now(self):
-        return (time.time() - self.start) * TICKS_PER_SECOND
+        return (time.time() - self.start) * self.simulation_speed
